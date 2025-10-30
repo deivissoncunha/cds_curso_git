e8c8c8a6fe8000549f17b8e3bf892be4f62cfc4e
Deivisson Cunha <deivissonlcunha@gmail.com>
2025-10-30 10:28:39 -0300

    adicionado resposta para a nona pergunta

--- src/answers.py
+++ src/answers.py
@@ -2,3 +2,24 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+def rd1_question_9(df):
+    df_grouped = df[["id", "seller_type"]].groupby("seller_type")
+
+    df_grouped = df_grouped.count().reset_index()
+
+    df_grouped = df_grouped.rename(columns={"id": "count"})
+
+    fig = px.bar(
+        df_grouped,
+        x="seller_type",
+        y="count",
+        labels={"seller_type": "Seller Type", "count": "Quantity"},
+        color="seller_type",
+        text="count",
+    )
+
+    fig.update_traces(textposition="outside")
+
+    st.plotly_chart(fig, use_container_width=True)
+
+    return None
